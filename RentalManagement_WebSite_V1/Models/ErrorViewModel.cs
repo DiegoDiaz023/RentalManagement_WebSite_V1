2f79573fb41fb6d4399e668be5a77062a39c125e
Diego <Diego@LAPTOP-Q2IMMA0M>
2023-03-20 23:52:13 -0400

    Add project files.

---
+++ RentalManagement_WebSite_V1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RentalManagement_WebSite_V1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
